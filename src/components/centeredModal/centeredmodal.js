11e2f1b123eea43993409676d6d3bf26abcd0692
rida-123 <ridashoaib666@gmail.com>
2022-04-21 13:31:35 -0700

    intergrate metamask using ether.js

--- src/components/centeredModal/centeredmodal.js
+++ src/components/centeredModal/centeredmodal.js
@@ -1,60 +1,52 @@
-
-import {ethers} from 'ethers'
-import  './centeredmodal.css';
+import { ethers } from 'ethers'
+import './centeredmodal.css';
 function MyVerticallyCenteredModal() {
-  const connectWallet=async()=>{
-  if(window.ethereum){
-  await window.ethereum.request({method:"eth_requestAccounts"})
-  .then(result=>{
-    accountChangeHandler(result[0])
-  })
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      console.log("Account:", await signer.getAddress());
+      let balance = await signer.getBalance();
+
+      console.log("balance: ", ethers.utils.formatEther(balance))
+      let network = await provider.getNetwork();
+      let chainId = network.chainId;
+      console.log("network:", network.name);
+      console.log("chainId: ", chainId);
+    }
+
+    else {
+      alert("please install MetaMask chrome extension")
+    }
   }
-  else{
-    alert("please install MetaMask chrome extension")
-  }
-  }
-  const accountChangeHandler=(newAccount)=>{
-    alert("Address is "+newAccount)
-    getUserBalance(newAccount.toString())
-  }
-  const chainChanged=()=>{
-    window.location.reload()
-  }
-  const getUserBalance=(address)=>{
-    window.ethereum.request({method:"eth_getBalance",params:[address,"latest"]})
-    .then(balance=>{
-      alert("Balance is "+ethers.utils.formatEther(balance))
-    })
-  }
-  window.ethereum.on("accountChanged",accountChangeHandler);
-  window.ethereum.on("chainChanged",chainChanged)
-    return (
-        <div
-        className="modal fade"
-        id="exampleModalCenter"
-        tabIndex={-1}
-        role="dialog"
-        aria-labelledby="exampleModalCenterTitle"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered" role="document">
-          <div className="modal-content">
-            <div className="modal-body" onClick={connectWallet}>
-                <center>
-            <img src="metamask.svg" width="50px" height="60px"></img>
-            <br></br>
-       
-            <h2 style={{paddingTop:"5px"}}>MetaMask</h2>
-          
-         
-            <p style={{color:"#A9A9BC",fontSize:"20px",paddingTop:"5px"}}>Connect to your MetaMask Wallet</p>
+  return (
+    <div
+      className="modal fade"
+      id="exampleModalCenter"
+      tabIndex={-1}
+      role="dialog"
+      aria-labelledby="exampleModalCenterTitle"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content">
+          <div className="modal-body" onClick={connectWallet}>
+            <center>
+              <img src="metamask.svg" width="50px" height="60px"></img>
+              <br></br>
+
+              <h2 style={{ paddingTop: "5px" }}>MetaMask</h2>
+
+
+              <p style={{ color: "#A9A9BC", fontSize: "20px", paddingTop: "5px" }}>Connect to your MetaMask Wallet</p>
             </center>
-            </div>
-          
           </div>
+
         </div>
       </div>
-      
-    );
-  }
-  export default MyVerticallyCenteredModal;
+    </div>
+
+  );
+}
+export default MyVerticallyCenteredModal;
